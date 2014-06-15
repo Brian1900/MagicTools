475c12aa295d60392bedfd6b2053209a18bfaa2d
Brian1900 <lwj2009@hotmail.com>
2014-06-15 13:48:52 +0800

    修改提交
    修改

---
+++ MagicalTools/classes/tools/汇率/BrianExchangeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrianExchangeViewController.h
+//  MagicalTools
+//
+//  Created by 陆 文杰 on 14-5-24.
+//  Copyright (c) 2014年 陆 文杰. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BrianExchangeViewController : BaseViewController
+
+@end
---
+++ MagicalTools/classes/tools/汇率/BrianExchangeViewController.m
@@ -0,0 +1,38 @@
+//
+//  BrianExchangeViewController.m
+//  MagicalTools
+//
+//  Created by 陆 文杰 on 14-5-24.
+//  Copyright (c) 2014年 陆 文杰. All rights reserved.
+//
+
+#import "BrianExchangeViewController.h"
+
+@interface BrianExchangeViewController ()
+
+@end
+
+@implementation BrianExchangeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
